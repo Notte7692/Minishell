5a7bbed764d4d2b28fd48167a7d1e57f8deb67a9
Nassim <nsalhi@student.42.fr>
2023-05-23 16:00:44 +0200

    push

--- main_bis.c
+++ main_bis.c
@@ -1,5 +1,63 @@
 #include "minishell.h"
 
+static int	ft_char_in_set(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	i;
+	size_t	start;
+	size_t	end;
+
+	if (!s1)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_char_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_char_in_set(s1[end - 1], set))
+		end--;
+	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = 0;
+	return (str);
+}
+
+char	**tab(char *av)
+{
+	int	i;
+	int	start;
+	int	end;
+	char	**tab;
+	char *str;
+	
+	str = ft_strtrim(av, " ");
+	i = 0;
+	while(av && av[i] && av[i] == ' ')
+		i++;
+	while(av && av[i])
+	{
+		start = i;
+		while (av[i] && av[i] >= 'a' && av[i] )
+	}
+}
+
 t_token *create_token_stack(char **av)
 {
     t_token *head = NULL;
@@ -26,10 +84,13 @@ t_token *create_token_stack(char **av)
         node->previous = NULL;
         node->next = NULL;
 
-        if (head == NULL) {
+        if (head == NULL)
+        {
             head = node;
             current = node;
-        } else {
+        }
+        else
+        {
             current->next = node;
             node->previous = current;
             current = node;
@@ -59,7 +120,7 @@ void free_token_stack(t_token *head) {
 }
 
 int main() {
-    char *av[] = {"command", "arg1", "<", "|", "env_var", NULL};
+    char *av[] = {"<command", "arg1", "<", "|", "env_var", NULL};
 
     t_token *token_stack = create_token_stack(av);
     print_token_stack(token_stack);
